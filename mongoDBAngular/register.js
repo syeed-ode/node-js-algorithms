e73c57a1f534d5f11ed9fd0932ed89fd095a20be
Syeed Ode <syeedode@Syeeds-MacBook-Pro.local>
2019-04-25 11:33:19 -0400

    Mongo DB addition

---
+++ mongoDBAngular/register.js
@@ -0,0 +1,49 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var mongojs = require('mongojs');
+var qs = require('querystring');
+
+var mimeTypes = {"html" : "text/html",
+    "jpeg": "image/jpeg",
+    "jpg":"image/jpeg",
+    "png":"image/png",
+    "js":"text/javascript",
+    "css":"text/css"
+};
+var databaseUrl = "test";
+var collections = ["testData"];
+var db = mongojs(databaseUrl, collections);
+var server = http.createServer(function onRequest(request, response) {
+    var urlParts = url.parse(request.url);
+    var fullPath = urlParts.pathname;
+    // var page = 'pages' + fullPath;
+    var page = 'pages' + fullPath;
+    var jsonUserObject = "";
+    if(fullPath === "/post") {
+        var userName = "";
+        request.on("data", function(chunk){
+            jsonUserObject = JSON.parse(chunk.toString());
+            userName = jsonUserObject.name;
+            userEmail = jsonUserObject.email;
+            db.testData.insert({name: userName, email:userEmail}, function(err,testData){
+                if(err || !testData) {
+                    console.log("Unable to add user");
+                }
+            });
+        });
+    }
+    var mimeType = mimeTypes[path.extname(page).split(".")[1]];
+    response.writeHead(200, mimeType);
+    fs.createReadStream(page).pipe(response);
+    // fs.exists(page, function fileExists(exists) {
+    //     if(exists) {
+    //         response.writeHead(200, mimeType);
+    //         fs.createReadStream(page).pipe(response);
+    //     } else {
+    //         response.write("Page Not Found");
+    //         response.end();
+    //     }
+    // });
+}).listen(3300);
